ce765b71005f53b79f7b2e38f8eefa5dae6720d3
MateoCostantini <101668925+MateoCostantini@users.noreply.github.com>
2022-04-07 10:44:07 -0300

    Add files via upload

---
+++ prueba_git_workshop.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  7 10:39:38 2022
+
+@author: 54911
+"""
+
+$ cat > main.py <<EOF
+# main.py
+if __name__ == '__main__':
+    print("Hola Mundo")
+EOF
